c71cc774a98548ee8323ab2fe7f7fc228d752ac6
Sarah69359 <sarah69359@estudante.ifb.edu.br>
2025-08-05 12:03:07 -0300

    Adicionando nova pagina de adicionar novo evento

---
+++ novo_evento.js
@@ -0,0 +1,15 @@
+const fotoInput = document.getElementById('foto');
+const fotoUploadDiv = document.querySelector('.foto-upload');
+
+fotoUploadDiv.addEventListener('click', () => {
+  fotoInput.click();
+});
+
+fotoInput.addEventListener('change', () => {
+  if (fotoInput.files.length > 0) {
+    // Mostrar nome do arquivo no lugar do "+"
+    fotoUploadDiv.textContent = fotoInput.files[0].name;
+  } else {
+    fotoUploadDiv.textContent = "+";
+  }
+});
