fb23b90fe42a95789ad0c427f34509bd38ff712f
Sarah69359 <sarah69359@estudante.ifb.edu.br>
2025-08-06 11:27:03 -0300

    Atualizando

--- novo_evento.js
+++ novo_evento.js
@@ -3,8 +3,13 @@ const fotoUploadDiv = document.querySelector('.foto-upload');
 
 fotoInput.addEventListener('change', () => {
   if (fotoInput.files.length > 0) {
-    // Mostrar nome do arquivo no lugar do "+"
-    fotoUploadDiv.textContent = fotoInput.files[0].name;
+
+    // Mostrar imagem do arquivo selecionado
+    const reader = new FileReader();
+    reader.onload = function(e) {
+    fotoUploadDiv.innerHTML = `<img src="${e.target.result}" style="max-width: 100%; max-height: 100%; border-radius: 8px;">`;
+};
+reader.readAsDataURL(fotoInput.files[0]);
   } else {
     fotoUploadDiv.textContent = "+";
   }
